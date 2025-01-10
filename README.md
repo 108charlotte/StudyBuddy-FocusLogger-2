4e7edbaa7a9fc3849a7d2a8ce9406ba491977def
108charlotte <108sarahsmith@gmail.com>
2025-01-10 16:23:57 -0500

    copied code snippet from codeshack

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ I often consulted the FLASK DOCUMENTATION, and used a code snippet from it along
 I used a Medium article by MOSIN INAMDAR titled "How to make a Signup, Login, and logout Route in flask app in 5 minutes" in order to model my login/registration system
 
 I then used a tutorial from CodeShack titled "Login System with Python Flask and MySQL" in order to create the backend
+
+COPILOT created the venv environment and gave me step-by-step instructions on how to set it up. I also used its step-by-step instructions on connecting MySQL to vs code. 
--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 # see README for how chatgpt was used in the creation of this code
 
-# imports other than Flask and render_template are from medium article (see README)
-from flask import Flask, render_template, request, redirect, session
+# imports other than Flask and render_template are not my own code (see README)
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 from werkzeug.security import generate_password_hash
 
 # Initialize the Flask application
 app = Flask(__name__)
 app.secret_key = "there_is_a_lopsided_octopus_somewhere_and_I_will_find_it"
+
+# code snippet from codeshack article
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():

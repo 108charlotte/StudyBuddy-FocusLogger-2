ca8d61471b76b7c22ca0ef7acb9a20cdd9d7487a
108charlotte <108sarahsmith@gmail.com>
2025-01-10 14:43:31 -0500

    Getting flask set up & creating authentication system

--- StudyBuddy-FocusLogger-2/README.md
+++ StudyBuddy-FocusLogger-2/README.md
@@ -1,4 +1,10 @@
 ## StudyBuddy
 
-## Flask application
-app.py - I had chatgpt generate me a basic python starter template, and then coded myself from there unless otherwise noted
+## Citations
+CHATGPT created the basic python starter template for app.py, along with some boilerplate html for my layout.html file
+
+I often consulted the FLASK DOCUMENTATION, and used a code snippet from it along with instructions to create a static folder and name the icon favicon.ico in order to add an icon to my webpage. I also consulted the flask documentation to implement jinja and create a layout.html
+
+I used a Medium article by MOSIN INAMDAR titled "How to make a Signup, Login, and logout Route in flask app in 5 minutes" in order to model my login/registration system
+
+I then used a tutorial from CodeShack titled "Login System with Python Flask and MySQL" in order to create the backend
--- StudyBuddy-FocusLogger-2/app.py
+++ StudyBuddy-FocusLogger-2/app.py
@@ -1,11 +1,13 @@
-# this is CHATGPT starter code for creating a basic flask application
+# see README for how chatgpt was used in the creation of this code
 
-from flask import Flask, render_template
+# imports other than Flask and render_template are from medium article (see README)
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash
 
 # Initialize the Flask application
 app = Flask(__name__)
+app.secret_key = "there_is_a_lopsided_octopus_somewhere_and_I_will_find_it"
 
-# Define a basic route
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -14,6 +16,19 @@ def hello_world():
 def login(): 
     return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register(): 
+    if request.method == 'GET': 
+        return render_template("register.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+        
+    if not (username and password): 
+        return render_template("register.html", message="Please fill out all fields.")
+
+    hashed_password = generate_password_hash(password)
+
+    return redirect("/login")
 
 # Run the application
 if __name__ == '__main__':
